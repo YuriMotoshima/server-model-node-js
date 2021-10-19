789772adf3a506a5581946ac34c2b34627f47373
YuriMotoshima <yurimotoshima@gmail.com>
2021-10-18 22:30:47 -0300

    Split routes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express')
+const express = require("express")
 const nunjucks = require("nunjucks")
-const routes = require('./routes')
+const routes = require("./routes")
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send("Hello!")
+server.use(express.static("public"))
+server.use(routes)
+
+server.set("view engine", "html")
+// server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
-server.listen(5000, function(){
+server.listen(5000, function () {
     console.log("Server is running")
-} )
+})
